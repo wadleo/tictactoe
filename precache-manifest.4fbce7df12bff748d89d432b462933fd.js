abd0b991eecb3a86a90e807d919344cd42b72573
Wadinga Leonard Ngonga <wadingaleonard@gmail.com>
2020-05-01 03:24:32 +0100

    Updates

---
+++ precache-manifest.4fbce7df12bff748d89d432b462933fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6276da76e62cb2b59fd64c19f0649bb1",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "a07502d45e6055537eb4",
+    "url": "/react-tictactoe/static/css/main.2afb2ac7.chunk.css"
+  },
+  {
+    "revision": "af919b444692c9230783",
+    "url": "/react-tictactoe/static/js/2.75d5abc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.75d5abc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07502d45e6055537eb4",
+    "url": "/react-tictactoe/static/js/main.c69068b1.chunk.js"
+  },
+  {
+    "revision": "70f3ab98cadcbc29ce8e",
+    "url": "/react-tictactoe/static/js/runtime-main.9d689b68.js"
+  }
+]);
